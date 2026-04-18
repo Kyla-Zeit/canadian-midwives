c67a525dd86424a1997544538cd61b5351be967a
Rebecca Maguire <112965597+Kyla-Zeit@users.noreply.github.com>
2026-04-18 02:29:42 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "node:path";
 
 export default defineConfig({
-  base: "/midwife-site1/",
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: "/canadian-midwives/",
+  plugins: [react()],
 });
